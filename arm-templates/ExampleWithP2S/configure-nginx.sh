90b3e50fe35e841cc22eff7b9b44d2067f2cefde
Ross Vernal <ross_vernal@hotmail.com>
2020-06-17 08:57:46 +0100

    Add chmod in to ensure the nginx config file has the right permissions

--- arm-templates/ExampleWithP2S/configure-nginx.sh
+++ arm-templates/ExampleWithP2S/configure-nginx.sh
@@ -4,6 +4,8 @@ export DEBIAN_FRONTEND=noninteractive
 rm /var/lib/apt/lists/* -vrf
 apt-get -y update
 apt-get -y install nginx
+chmod -R 766 /etc/nginx/nginx.conf
 echo "stream { upstream postgresql { server $1.postgres.database.azure.com:5432; } server { listen 5432; proxy_pass postgresql; } }" >> /etc/nginx/nginx.conf
+chmod -R 644 /etc/nginx/nginx.conf
 service nginx restart
 update-rc.d nginx defaults
